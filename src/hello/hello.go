5633b3119c7b1c43827690c67c3fa767bc499b35
yorlysoro <yorlysoro@gmail.com>
2020-04-06 16:36:17 -0400

    Trabajando con paquetes

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/yorlysoro/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG, olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
